2aeaa5911dd8992233cb71f94e47e960a2ee64af
Vishwas <vishwas7906@gmail.com>
2026-08-07 23:00:40 +0530

    initial commit

---
+++ Count Salary Categories.sql
@@ -0,0 +1,11 @@
+SELECT 'Low Salary' AS category,
+SUM(IF(income < 20000, 1, 0)) AS accounts_count
+FROM accounts
+UNION
+SELECT 'Average Salary' AS category,
+SUM(IF(income BETWEEN 20000 AND 50000, 1, 0)) AS accounts_count
+FROM accounts
+UNION
+SELECT 'High Salary' AS category,
+SUM(IF(income > 50000, 1, 0)) AS accounts_count
+FROM accounts;
---
+++ Exchange seats.sql
@@ -0,0 +1,9 @@
+SELECT
+    CASE
+        WHEN id = (SELECT MAX(id) FROM Seat) AND id % 2 = 1 THEN id
+        WHEN id % 2 = 1 THEN id + 1
+        ELSE id - 1
+    END AS id,
+    student
+FROM Seat
+ORDER BY id;
